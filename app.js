167fcba8521a55b54cfe246b1d1c00d810a636f4
Equipo <juanignacioleyes@gmail.com>
2021-04-19 08:57:25 -0300

    creé el index.html y la hoja de estilos

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const publicPath = path.resolve(__dirname, "public");
 app.use(express.static(publicPath));
 
 app.get("/", (req, res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/home.html"));
+    res.sendFile(path.resolve(__dirname,"./views/index.html"));
 });
 
 app.listen(3050, ()=>{
