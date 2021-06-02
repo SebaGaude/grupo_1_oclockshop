0730d116141c14fb8ad17bd7991d0cd75fa1fcee
DamianRMansilla <damianrmansilla85@gmail.com>
2021-06-02 16:55:05 +0200

    commit para poder hacer pull

--- src/routes/products.js
+++ src/routes/products.js
@@ -5,7 +5,7 @@ const productsController = require("../controllers/productsController");
 
 const app = express();
 
-const publicPath = path.resolve(__dirname, "./public");
+const publicPath = path.resolve(__dirname, "../public");
 app.use(express.static(publicPath));
 
 router.get("/", productsController.products);
