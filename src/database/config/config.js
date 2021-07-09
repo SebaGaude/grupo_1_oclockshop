394f85312839a51a1a73813b79ce87e8af038d49
juaniribarren <juanmanuelif@hotmail.com>
2021-07-09 19:02:28 -0300

    sequelize V2

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,8 +1,8 @@
-{
-  "development": {
+module.exports = { // el module.exports se agrega porque este archivo sino es un JSON y asi lo convertimos en un JS
+  "development": { //  a configurar el username, la contraseña si tuviera, la base de datos y quizas sea necesario agregar el puerto
     "username": "root",
     "password": null,
-    "database": "database_development",
+    "database": "oclockshop",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
