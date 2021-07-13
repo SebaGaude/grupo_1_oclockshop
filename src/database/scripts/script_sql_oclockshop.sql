6e128e981c8379600b33e5f486ac2a1085742790
juaniribarren <juanmanuelif@hotmail.com>
2021-07-13 17:49:31 -0300

    nueva version DER

--- src/database/scripts/script_sql_oclockshop.sql
+++ src/database/scripts/script_sql_oclockshop.sql
@@ -5,6 +5,9 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema oclockshop
 -- -----------------------------------------------------
 
@@ -15,110 +18,126 @@ CREATE SCHEMA IF NOT EXISTS `oclockshop` DEFAULT CHARACTER SET utf8 COLLATE utf8
 USE `oclockshop` ;
 
 -- -----------------------------------------------------
--- Table `oclockshop`.`usuario`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oclockshop`.`usuario` (
-  `idusuario` INT NOT NULL,
-  `nombre` VARCHAR(45) NOT NULL,
-  `apellido` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `imagen` VARCHAR(45) NOT NULL,
-  `contraseña` VARCHAR(20) NOT NULL,
-  PRIMARY KEY (`idusuario`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `oclockshop`.`categoria`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `oclockshop`.`categoria` (
-  `idcategoria` INT NOT NULL,
-  `nombre` VARCHAR(45) NOT NULL,
-  `descripcion` VARCHAR(45) NULL,
-  PRIMARY KEY (`idcategoria`))
-ENGINE = InnoDB;
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(255) NOT NULL,
+  `descripcion` TEXT(10000) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `nombre_UNIQUE` (`nombre` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_unicode_ci;
 
 
 -- -----------------------------------------------------
 -- Table `oclockshop`.`marca`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `oclockshop`.`marca` (
-  `idmarca` INT NOT NULL,
-  `nombre` VARCHAR(45) NOT NULL,
-  `descripcion` VARCHAR(45) NULL,
-  PRIMARY KEY (`idmarca`))
-ENGINE = InnoDB;
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(255) NOT NULL,
+  `descripcion` TEXT(10000) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `nombre_UNIQUE` (`nombre` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_unicode_ci;
 
 
 -- -----------------------------------------------------
 -- Table `oclockshop`.`producto`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `oclockshop`.`producto` (
-  `idproducto` INT NOT NULL,
-  `articulo` VARCHAR(45) NOT NULL,
-  `descripcion` VARCHAR(45) NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `articulo` VARCHAR(255) NOT NULL,
+  `descripcion` VARCHAR(255) NOT NULL,
   `id_categoria` INT NOT NULL,
   `id_marca` INT NOT NULL,
   `stock` INT NOT NULL,
-  `precio` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idproducto`),
-  INDEX `id_categoria_idx` (`id_categoria` ASC) VISIBLE,
-  INDEX `id_marca_idx` (`id_marca` ASC) VISIBLE,
+  `precio` DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `id_categoria_idx` (`id_categoria` ASC),
+  INDEX `id_marca_idx` (`id_marca` ASC),
+  UNIQUE INDEX `articulo_UNIQUE` (`articulo` ASC),
   CONSTRAINT `id_categoria`
     FOREIGN KEY (`id_categoria`)
-    REFERENCES `oclockshop`.`categoria` (`idcategoria`)
+    REFERENCES `oclockshop`.`categoria` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `id_marca`
     FOREIGN KEY (`id_marca`)
-    REFERENCES `oclockshop`.`marca` (`idmarca`)
+    REFERENCES `oclockshop`.`marca` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_unicode_ci;
+
+
+-- -----------------------------------------------------
+-- Table `oclockshop`.`usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `oclockshop`.`usuario` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(255) NOT NULL,
+  `apellido` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `imagen` VARCHAR(255) NOT NULL,
+  `contraseña` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_unicode_ci;
 
 
 -- -----------------------------------------------------
 -- Table `oclockshop`.`venta`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `oclockshop`.`venta` (
-  `idventa` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `fecha` DATETIME NOT NULL,
   `total` DECIMAL(10,2) NOT NULL,
-  `id_usuario` INT NULL,
-  PRIMARY KEY (`idventa`),
-  INDEX `id_usuario_idx` (`id_usuario` ASC) VISIBLE,
+  `id_usuario` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `id_usuario_idx` (`id_usuario` ASC),
   CONSTRAINT `id_usuario`
     FOREIGN KEY (`id_usuario`)
-    REFERENCES `oclockshop`.`usuario` (`idusuario`)
+    REFERENCES `oclockshop`.`usuario` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_unicode_ci;
 
 
 -- -----------------------------------------------------
 -- Table `oclockshop`.`detalle_venta`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `oclockshop`.`detalle_venta` (
-  `iddetalle_venta` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `id_producto` INT NOT NULL,
   `id_venta` INT NOT NULL,
   `precio` DECIMAL(10,2) NOT NULL,
   `cantidad` INT NOT NULL,
-  `subtotal` DECIMAL(10,2) NULL,
-  PRIMARY KEY (`iddetalle_venta`),
-  INDEX `id_producto_idx` (`id_producto` ASC) VISIBLE,
-  INDEX `id_venta_idx` (`id_venta` ASC) VISIBLE,
+  `subtotal` DECIMAL(10,2) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `id_producto_idx` (`id_producto` ASC),
+  INDEX `id_venta_idx` (`id_venta` ASC),
   CONSTRAINT `id_producto`
     FOREIGN KEY (`id_producto`)
-    REFERENCES `oclockshop`.`producto` (`idproducto`)
+    REFERENCES `oclockshop`.`producto` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `id_venta`
     FOREIGN KEY (`id_venta`)
-    REFERENCES `oclockshop`.`venta` (`idventa`)
+    REFERENCES `oclockshop`.`venta` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_unicode_ci;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
