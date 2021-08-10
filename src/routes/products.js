14f5dd2c302abfdb25253e822ea951e6cfb44daa
SebaGaude <gauadellisebastian@yahoo.com>
2021-08-09 23:41:50 -0300

    require middleware

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 const productsController = require("../controllers/productsController");
+
+// middlewares
+const createEditProductMiddleware = require("../middlewares/createEditProductMiddleware");
+
 const multer = require("multer");
 
 //---------------MULTER-----------------------
