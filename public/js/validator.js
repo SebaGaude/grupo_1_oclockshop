b36dd917f9835eb5c452a421fed05c137a8224dc
DamianRMansilla <damianrmansilla85@gmail.com>
2021-08-15 16:00:35 +0200

    arreglo validaciones front register

--- public/js/validator.js
+++ public/js/validator.js
@@ -10,7 +10,7 @@ window.addEventListener("load", function(){
     //})
 
 
-    //form.nombre.focus();
+    
 
     form.addEventListener("submit", function(e){
         let errors = [];
@@ -20,52 +20,73 @@ window.addEventListener("load", function(){
         let imagen = document.querySelector("#imagen");
         let contraseña = document.querySelector("#contraseña");
 
+        //nombre.focus();
 
-        if(nombre.value.length < 1){
-            nombre.style.border = "solid 3px #842029";
-            nombre.style.color = "#842029";
-            errors.push("Completá tu nombre");  
+        if(nombre.value == ""){
+            nombre.classList.add("is-invalid");
+            nombre.classList.remove("is-valid");
+            nombre.classList.remove("register-input");
+            errors.push("Completá tu nombre"); 
         }else{
-            nombre.style.border = "solid 3px #198754";
-            nombre.style.color = "#198754";
+            nombre.classList.add("is-valid");
+            nombre.classList.remove("is-invalid");
+            nombre.classList.remove("register-input");
             apellido.focus();
         }
-        if(apellido.value.length < 1){
-            apellido.style.border = "solid 3px #842029";
-            apellido.style.color = "#842029";
+        if(apellido.value == ""){
+            apellido.classList.add("is-invalid");
+            apellido.classList.remove("is-valid");
+            apellido.classList.remove("register-input");
             errors.push("Completá tu apellido");  
         }else{
+            apellido.classList.add("is-valid");
+            apellido.classList.remove("is-invalid");
+            apellido.classList.remove("register-input");
             email.focus();
         }
-        if(email.value.length < 1){
-            email.style.border = "solid 3px #842029";
-            email.style.color = "#842029";
+        if(email.value == ""){
+            email.classList.add("is-invalid");
+            email.classList.remove("is-valid");
+            email.classList.remove("register-input");
             errors.push("Completá tu email");  
         }else{
+            email.classList.add("is-valid");
+            email.classList.remove("is-invalid");
+            email.classList.remove("register-input");
             imagen.focus();
         }
-        if(imagen.value.length < 1){
-            imagen.style.border = "solid 3px #842029";
-            imagen.style.color = "#842029";
+        if(imagen.value == ""){
+            imagen.classList.add("is-invalid");
+            imagen.classList.remove("is-valid");
+            imagen.classList.remove("register-input");
             errors.push("Elegí una imágen");  
         }else{
+            imagen.classList.add("is-valid");
+            imagen.classList.remove("is-invalid");
+            imagen.classList.remove("register-input");
             contraseña.focus();
         }
-        if(contraseña.value.length < 1){
-            contraseña.style.border = "solid 3px #842029";
-            contraseña.style.color = "#842029";
+        if(contraseña.value == ""){
+            contraseña.classList.add("is-invalid");
+            contraseña.classList.remove("is-valid");
+            contraseña.classList.remove("register-input");
             errors.push("Ingresá una contraseña");  
         }else{
+            contraseña.classList.add("is-valid");
+            contraseña.classList.remove("is-invalid");
+            contraseña.classList.remove("register-input");
             contraseña.focus();
         }
 
         //Determinar si hay o no errores - Mostrarlos al usuario
-
+        console.log(errors);
         if(errors.length > 0){
             e.preventDefault();
+
             let msjError = document.querySelector("#error-nombre");
             //msjError.classList.add("alert-warning");
             msjError.innerHTML = "";
+
             for (let i = 0; i < errors.length; i++) {
                 msjError.innerHTML += `<li>${errors[i]}</li>`   
             }
