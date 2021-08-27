eb76d5b7bc153c92b659672db0e1966ba7b2e948
SebaGaude <gaudellisebastian@yahoo.com>
2021-08-26 21:02:54 -0300

    atualizacion validaciones front

--- src/middlewares/adminMiddleware.js
+++ src/middlewares/adminMiddleware.js
@@ -3,7 +3,6 @@ y quiere editar o crear un producto
 lo redirecciona al index */
 
 function adminMiddleware(req, res, next) {
-        console.log(req.session.usuarioLogueado)
 	    if (req.session.usuarioLogueado == undefined || (req.session.usuarioLogueado && req.session.usuarioLogueado.perfil != 'Administrador')) {
 		    return res.redirect('/');
 	    }
