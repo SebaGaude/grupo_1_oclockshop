9e2862e37123a203045dbb4ad0b2cb1ece285db8
tomasmersan <81237811+tomasmersan@users.noreply.github.com>
2021-08-30 15:26:14 -0400

    vista 'nuestro equipo'

--- public/js/validateWorkWithUs.js
+++ public/js/validateWorkWithUs.js
@@ -1,40 +1,22 @@
-// // Validacion front register //
+// // Validacion Trabaja con Nosotros //
 
-const form = document.querySelector(".form-login-register");
-const nombre = document.querySelector("#nombre");
-const apellido = document.querySelector("#apellido");
-const email = document.querySelector("#email");
-const imagen = document.querySelector("#imagen");
-const contraseña = document.querySelector("#contraseña");
+const form = document.querySelector(".form-extras");
+const email = document.querySelector(".email-workwitus");
+const submit = document.querySelector(".boton-crear-cuenta")
 
 form.addEventListener("submit", (e)=>{
-    
-    
+
+    alert("¡Gracias por enviarnos tu CV! Nos comunicaremos contigo en la brevedad");
+
     checkInputs();
-
-
 
     function checkInputs(){
         //get the values from the inputs
-        const nombreUsuario = nombre.value.trim();
-        const apellidoUsuario = apellido.value.trim();
+        
         const emailUsuario = email.value.trim();
-        const imagenUsuario = imagen.value.trim();
-        const contraUsuario = contraseña.value.trim();
+        
         const errors = []
         
-        if(nombreUsuario === ""){
-            setErrorFor(nombre, "Debes completar tu nombre")
-            errors.push("error en nombreUsuario");
-        }else{
-            setSuccessFor(nombre);
-        }
-        if(apellidoUsuario === ""){
-            setErrorFor(apellido, "Debes completar tu apellido")
-            errors.push("error en apellidoUsuario");
-        }else{
-            setSuccessFor(apellido);
-        }
         if(emailUsuario === ""){
             setErrorFor(email, "Debes completar tu email")
             errors.push("error en email");
@@ -44,22 +26,7 @@ form.addEventListener("submit", (e)=>{
         }else{
             setSuccessFor(email);
         }
-        if(imagenUsuario === ""){
-            setErrorFor(imagen, "Debes elegir una imágen de perfil")
-            errors.push("error en imagen");
-        }else{
-            setSuccessFor(imagen);
-        }
-        if(contraUsuario === ""){
-            setErrorFor(contraseña, "Debes elegir una contraseña")
-            errors.push("error en password");
-        }else if(!isPassword(contraUsuario)){
-            setErrorFor(contraseña, "La contraseña debe tener como minimo 8 caracteres, incluyendo una mayuscula, una minuscula, un numero y alguno de los siguientes caracteres #?!@$%^&*-")
-            errors.push("error en password");
-        }else{
-            setSuccessFor(contraseña);
-        }
-        
+            
         function setErrorFor(input, message){
             const tryDiv = input.parentElement; // .try class from div
             const small = tryDiv.querySelector("small");
