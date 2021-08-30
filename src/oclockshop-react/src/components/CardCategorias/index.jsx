fc1ef942bd0ef7a730d44b2b355bbe434dfa0029
DamianRMansilla <damianrmansilla85@gmail.com>
2021-08-30 03:20:18 +0200

    arreglo categorias dashboard

--- src/oclockshop-react/src/components/CardCategorias/index.jsx
+++ src/oclockshop-react/src/components/CardCategorias/index.jsx
@@ -16,7 +16,6 @@ class CardCategorias extends Component {
         const getData = async() => {
             let response = await fetch("http://localhost:3050/api/categorias");
             let data = await response.json();
-            console.log
             return data;     
         }
 
@@ -38,20 +37,25 @@ class CardCategorias extends Component {
         const {isLoading, categorias} = this.state;
         return(
             <div>
-                <h2 className="h2">Listado de categorias</h2>
                 { isLoading && <h4>Cargando...</h4> }
-                <div className="img">
-                { categorias.map( unaCategoria=> {
-                    return(
-                        <div key={unaCategoria.dataValues.id} >
-                            
-                            <h3> {unaCategoria.dataValues.nombre} </h3>
-                            
-                        </div>
-                    )
-                })}
-                </div>
-
+                        { categorias.map( unaCategoria=> {
+                        return(
+                            <div className="card-body">
+                                <div className="row">
+                                    <div className="col-lg-6 mb-4">
+                                        <div className="card bg-dark text-white shadow">
+                                            <div className="card-body">
+                                                <div className="img">
+                                                    <div key={unaCategoria.dataValues.id} >
+                                                        <p> {unaCategoria.dataValues.nombre} </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+							</div>
+                        )})}
             </div>
         )
     }
